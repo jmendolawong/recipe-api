683e48df71b536b83ef6bfed6b322cdfc99d359f
mendolawong <jwong.ncsu@gmail.com>
2020-02-07 16:17:11 -0500

    Skeleton deploy

--- src/app.js
+++ src/app.js
@@ -5,21 +5,52 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV, CLIENT_ORIGIN } = require('./config.js')
 
+// Init app to express object
 const app = express();
 
+/***********  Middleware ***********/
+// Helmet to hide sensitive info in res header
+// To be placed *before* CORS according to bloc content
+app.use(helmet())
+
+// For cross domain access
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 )
 
+// Logging middleware, mostly for dev env
 const morganSetting = NODE_ENV === 'production' ? 'tiny' : 'dev';
 app.use(morgan(morganSetting))
-app.use(helmet())
+
+// Authorization
+app.use(function validateBearerToken(req, res, next) {
+  // Server-side
+  const apiToken = process.env.API_TOKEN;
+  // Client supplied
+  const authToken = req.get('Authorization');
+
+  // If client doesn't supply or the their token doesn't match
+  // serverside, then error 'unauthorized access'
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({
+      error: { message: 'Unauthorized access' }
+    })
+  }
+  next();
+})
+
+
+/***********  Endpoints ***********/
+
+app.get('/api/*', (req, res) => {
+  res.json({ "test": "works!" });
+});
 
 
 
-
+/***********  Error Handling ***********/
 app.use((error, req, res, next) => {
   let response;
   if (NODE_ENV === 'production') {
--- src/server.js
+++ src/server.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./app')
+const { PORT } = require('./config.js')
 
-const PORT = process.env.PORT || 8000;
-
-app.get('/api/*', (req, res) => {
-  res.json({ ok: true });
-});
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
